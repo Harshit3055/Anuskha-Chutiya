3854e1ed3db1e6a5a65fee029ab5568bf2f9f8d0
Harshit3055 <76194709+Harshit3055@users.noreply.github.com>
2021-02-20 21:00:08 +0530

    Yeh Dekh BSDK

---
+++ Pattern4.java
@@ -0,0 +1,15 @@
+// WAP to Print the following Pattern
+/*
+* 1        5
+*   2    4
+*      3
+*   2    4
+* 1         5
+* */
+public class Pattern4 {
+    public static void main(String args[])
+    {
+        int i,j,k;
+
+    }
+}
